dc9a1718cf1792eeac8307736a8a76e502ee2b14
masamiz <maruo@masamiz.com>
2023-05-18 16:06:19 +0900

    first commit

--- pages/page_4.py
+++ pages/page_4.py
@@ -1,12 +1,26 @@
 import streamlit as st
 import pandas as pd
+import PyPDF2
 import openpyxl
+
 # データ分析
 df = pd.read_csv('./data/data2.csv', index_col='日付')
 st.line_chart(df)
 st.bar_chart(df)
 
+st.title("PDF Viewer")
+uploaded_file = st.file_uploader("Choose a PDF file", type="pdf")
 
+if uploaded_file is not None:
+    pdf_reader = PyPDF2.PdfFileReader(uploaded_file)
+    num_pages = pdf_reader.numPages
+    st.write(f"Number of pages: {num_pages}")
+
+    for page_number in range(1, num_pages + 1):
+        page = pdf_reader.getPage(page_number - 1)
+        text = page.extractText()
+        st.subheader(f"Page {page_number}")
+        st.write(text)
 def main():
     st.title("ファイルを読み込んでデータフレームを表示する")
 
@@ -20,7 +34,7 @@ def main():
             if file_extension == "csv":
                 df2 = pd.read_csv(file, encoding="utf-8")
             elif file_extension == "xlsx":
-                df2 = pd.read_excel(file)#, encoding="openpyxl")
+                df2 = pd.read_excel(file)  # , encoding="openpyxl")
 
             st.write("データフレームの内容:")
             st.dataframe(df2)
